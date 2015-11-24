0946de9a4bb92ee5a5f5c792fad8fb021a5fd321
Florian Kolb <florian.kolb@mni.thm.de>
2015-11-24 20:52:12 +0100

    added: dummy data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,17 +2,39 @@ angular.module('newcomer.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-// .controller('PlaylistsCtrl', function($scope) {
-//   $scope.playlists = [
-//     { title: 'Reggae', id: 1 },
-//     { title: 'Chill', id: 2 },
-//     { title: 'Dubstep', id: 3 },
-//     { title: 'Indie', id: 4 },
-//     { title: 'Rap', id: 5 },
-//     { title: 'Cowbell', id: 6 }
-//   ];
-// })
-//
-// .controller('PlaylistCtrl', function($scope, $stateParams) {
-// });
+ .controller('EventCtrl', function($scope) {
+   $scope.events = [
+     { title: 'Ausflug zum Schiffenberg', date: '23.11.2015', location: 'Haupteingang Camp', id: 1 },
+     { title: 'Spendenverteilung', date: '06.10.2015', location: 'Camp Zentrale',  id: 2 },
+     { title: 'Essensausgabe', date: '29.09.2015', location: 'Camp Zentrale',  id: 3 },
+     { title: 'Filmeabend', date: '14.09.2015', location: 'Gemeinschaftsraum',  id: 4 },
+     { title: 'Spieleabend', date: '30.08.2015', location: 'Gemeinschaftsraum',  id: 5 },
+     { title: 'Stadt Gießen kennen lernen', date: '10.07.2015', location: 'Haupteingang Camp',  id: 6 }
+   ];
+ });
+
+ .controller ('MapCtrl', function($scope, $ionicLoading){
+   google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(50.592205, 8.732367);
+
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+
+        $scope.map = map;
+ })
+ });
 });
